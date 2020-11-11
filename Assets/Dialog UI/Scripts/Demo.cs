fb3d9ab7c66c443a98eb102681b4e8791ad98b32
Hamza HERBOU <hamza95herbou@gmail.com>
2020-11-11 13:49:38 +0100

    add files

---
+++ Assets/Dialog UI/Scripts/Demo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using EasyUI.Dialogs;
+
+public class Demo : MonoBehaviour {
+
+	void Start ( ) {
+
+		// First Dialog -----------------------------
+		DialogUI.Instance
+		.SetTitle ( "Message 1" )
+		.SetMessage ( "Hello!" )
+		.SetButtonColor ( DialogButtonColor.Blue )
+		.OnClose ( ( ) => Debug.Log ( "Closed 1" ) )
+		.Show ( );
+
+
+		// Second Dialog ----------------------------
+		DialogUI.Instance
+		.SetTitle ( "Message 2" )
+		.SetMessage ( "Hello Again :)" )
+		.SetButtonColor ( DialogButtonColor.Magenta )
+		.SetButtonText ( "ok" )
+		.OnClose ( ( ) => Debug.Log ( "Closed 2" ) )
+		.Show ( );
+
+
+		// Third Dialog -----------------------------
+		DialogUI.Instance
+		.SetTitle ( "Message 3" )
+		.SetMessage ( "Bye!" )
+		.SetFadeInDuration ( 1f )
+		.SetButtonColor ( DialogButtonColor.Red )
+		.OnClose ( ( ) => Debug.Log ( "Closed 3" ) )
+		.Show ( );
+
+	}
+
+}
---
+++ Assets/Dialog UI/Scripts/DialogUI.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EasyUI.Dialogs {
+
+	public enum DialogButtonColor {
+		Black,
+		Purple,
+		Magenta,
+		Blue,
+		Green,
+		Yellow,
+		Orange,
+		Red
+	}
+
+	public class Dialog {
+		public string Title = "Title";
+		public string Message = "Message goes here.";
+		public string ButtonText = "Close";
+		public float FadeInDuration = .3f;
+		public DialogButtonColor ButtonColor = DialogButtonColor.Black;
+		public UnityAction OnClose = null;
+	}
+
+	public class DialogUI : MonoBehaviour {
+		[SerializeField] GameObject canvas;
+		[SerializeField] Text titleUIText;
+		[SerializeField] Text messageUIText;
+		[SerializeField] Button closeUIButton;
+
+		Image closeUIButtonImage;
+		Text closeUIButtonText;
+		CanvasGroup canvasGroup;
+
+		[Space ( 20f )]
+		[Header ( "Close button colors" )]
+		[SerializeField] Color[] buttonColors;
+
+		Queue<Dialog> dialogsQueue = new Queue<Dialog> ( );
+		Dialog dialog = new Dialog ( );
+		Dialog tempDialog;
+
+		[HideInInspector] public bool IsActive = false;
+
+		//Singleton pattern
+		public static DialogUI Instance;
+
+
+
+		void Awake ( ) {
+			Instance = this;
+
+			closeUIButtonImage = closeUIButton.GetComponent <Image> ( );
+			closeUIButtonText = closeUIButton.GetComponentInChildren <Text> ( );
+			canvasGroup = canvas.GetComponent <CanvasGroup> ( );
+
+			//Add close event listener
+			closeUIButton.onClick.RemoveAllListeners ( );
+			closeUIButton.onClick.AddListener ( Hide );
+		}
+
+		
+		public DialogUI SetTitle ( string title ) {
+			dialog.Title = title;
+			return Instance;
+		}
+
+		
+		public DialogUI SetMessage ( string message ) {
+			dialog.Message = message;
+			return Instance;
+		}
+
+		
+		public DialogUI SetButtonText ( string text ) {
+			dialog.ButtonText = text;
+			return Instance;
+		}
+
+		
+		public DialogUI SetButtonColor ( DialogButtonColor color ) {
+			dialog.ButtonColor = color;
+			return Instance;
+		}
+
+		
+		public DialogUI SetFadeInDuration ( float duration ) {
+			dialog.FadeInDuration = duration;
+			return Instance;
+		}
+
+		
+		public DialogUI OnClose ( UnityAction action ) {
+			dialog.OnClose = action;
+			return Instance;
+		}
+
+		//-------------------------------------
+		
+		public void Show ( ) {
+			dialogsQueue.Enqueue ( dialog );
+			//Reset Dialog
+			dialog = new Dialog ( );
+
+			if ( !IsActive )
+				ShowNextDialog ( );
+		}
+
+
+		void ShowNextDialog ( ) {
+			tempDialog = dialogsQueue.Dequeue ( );
+
+			titleUIText.text = tempDialog.Title;
+			messageUIText.text = tempDialog.Message;
+			closeUIButtonText.text = tempDialog.ButtonText.ToUpper ( );
+			closeUIButtonImage.color = buttonColors [ ( int )tempDialog.ButtonColor ];
+
+			canvas.SetActive ( true );
+			IsActive = true;
+			StartCoroutine ( FadeIn ( tempDialog.FadeInDuration ) );
+		}
+
+
+		// Hide dialog
+		public void Hide ( ) {
+			canvas.SetActive ( false );
+			IsActive = false;
+
+			// Invoke OnClose Event
+			if ( tempDialog.OnClose != null )
+				tempDialog.OnClose.Invoke ( );
+
+			StopAllCoroutines ( );
+
+			if ( dialogsQueue.Count != 0 )
+				ShowNextDialog ( );
+		}
+
+
+		//-------------------------------------
+
+		IEnumerator FadeIn ( float duration ) {
+			float startTime = Time.time;
+			float alpha = 0f;
+
+			while ( alpha < 1f ) {
+				alpha = Mathf.Lerp ( 0f, 1f, (Time.time - startTime) / duration );
+				canvasGroup.alpha = alpha;
+
+				yield return null;
+			}
+		}
+	}
+
+}
